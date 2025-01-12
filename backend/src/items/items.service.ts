a1aeda99f31cdd39b5139d3d1ee9f0a42b1e0502
Zafar17 <zafariqbal.zafi@gmail.com>
2025-01-12 21:37:01 +0500

    feat: added item service

--- backend/src/items/items.service.ts
+++ backend/src/items/items.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Item } from './item.entity';
 
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+    constructor(
+        @InjectRepository(Item)
+        private itemsRepository: Repository<Item>,
+    ) { }
+
+    async createItem(itemData: {
+        name: string;
+        description: string;
+        startingPrice: number;
+        auctionEndTime: Date;
+    }): Promise<Item> {
+        const item = this.itemsRepository.create(itemData);
+        return this.itemsRepository.save(item);
+    }
+
+    async getItems(): Promise<Item[]> {
+        return this.itemsRepository.find();
+    }
+
+    async getItemById(id: number): Promise<Item> {
+        return this.itemsRepository.findOneBy({ id });
+    }
+}
