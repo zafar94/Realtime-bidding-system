3c47a0deb6d5a4695f42388729779ce6d8a5b4b1
Zafar17 <zafariqbal.zafi@gmail.com>
2025-01-12 21:30:10 +0500

    feat: updated app.module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Item } from './entities/item.entity';
+import { Bid } from './entities/bid.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: 'user',
+      password: 'password',
+      database: 'bidding',
+      entities: [User, Item, Bid],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([User, Item, Bid]),
+  ],
 })
-export class AppModule {}
+export class AppModule { }
