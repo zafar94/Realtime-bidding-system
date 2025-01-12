2b4256dbfb07819684c5ab54bc51e110c0eb6ae0
Zafar17 <zafariqbal.zafi@gmail.com>
2025-01-12 21:37:27 +0500

    feat: added item controller

--- backend/src/items/items.controller.ts
+++ backend/src/items/items.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { ItemsService } from './items.service';
+import { Item } from './item.entity';
 
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+    constructor(private readonly itemsService: ItemsService) { }
+
+    @Post()
+    async createItem(@Body() itemData: { name: string; description: string; startingPrice: number; auctionEndTime: Date }): Promise<Item> {
+        return this.itemsService.createItem(itemData);
+    }
+
+    @Get()
+    async getItems(): Promise<Item[]> {
+        return this.itemsService.getItems();
+    }
+
+    @Get(':id')
+    async getItemById(@Param('id') id: number): Promise<Item> {
+        return this.itemsService.getItemById(id);
+    }
+}
