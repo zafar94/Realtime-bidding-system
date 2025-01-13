c71af317c55cdca920174ebb5af5b9e1ca21e2a9
Zafar17 <zafariqbal.zafi@gmail.com>
2025-01-14 00:48:19 +0500

    feat: added item

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ const App: React.FC = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/auction/:id" element={<AuctionDetail />} />
+        <Route path="/auction/:itemId" element={<AuctionDetail />} />
         <Route path="/create-auction" element={<CreateAuction />} />
       </Routes>
     </Router>
