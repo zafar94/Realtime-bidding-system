dcdcd9a23fce4a708cd9bafbbb66bbec559f8c6d
Zafar17 <zafariqbal.zafi@gmail.com>
2025-01-14 00:14:28 +0500

    feat: added auction routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import AuctionDetail from './components/AuctionDetail';
+import CreateAuction from './components/CreateAuction';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/auction/:itemId" element={<AuctionDetail />} />
+        <Route path="/auction/:id" element={<AuctionDetail />} />
+        <Route path="/create-auction" element={<CreateAuction />} />
       </Routes>
     </Router>
   );
