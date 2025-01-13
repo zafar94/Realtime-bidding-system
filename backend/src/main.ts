01431ee8d26fc4747c01fc56b7c4b3796dd53aa4
Zafar17 <zafariqbal.zafi@gmail.com>
2025-01-13 19:04:06 +0500

    feat: add main

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  try {
+    const app = await NestFactory.create(AppModule);
+
+    const configService = app.get(ConfigService);
+    const port = configService.get<number>('PORT') || 3001;
+
+    await app.listen(port);
+    console.log(`Application is running on: PORT:${port}`);
+  } catch (error) {
+    console.error('Error during application bootstrap', error);
+    process.exit(1);
+  }
 }
 bootstrap();
