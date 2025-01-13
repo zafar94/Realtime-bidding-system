00947e47c23f9c36a2e2826e32bb3002212025ab
Zafar17 <zafariqbal.zafi@gmail.com>
2025-01-13 17:40:45 +0500

    feat: add DB configs

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,18 +5,26 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Item } from './items/item.entity';
 import { Bid } from './bids/bid.entity';
 import { AuctionGateway } from './auction/auction.gateway';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'postgres',
-      port: 5432,
-      username: 'auction_user',
-      password: 'auction_pass',
-      database: 'auction_db',
-      entities: [Item, Bid],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USER'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        entities: [Item, Bid],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
 
     ItemsModule,
