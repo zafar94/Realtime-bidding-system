26978dfd64f3e03bd572dde70ca8ba24badbd8af
Zafar17 <zafariqbal.zafi@gmail.com>
2025-01-13 22:17:20 +0500

    added seed service in user module

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { SeedService } from './seed.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])]
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [SeedService],
+  exports: [SeedService],
 })
 export class UsersModule { }
